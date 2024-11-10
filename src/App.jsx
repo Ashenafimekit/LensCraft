f21023ead5ca843b4329cb4248909a90e99a882c
Ashenafi Mekit <ashumekit502@gmail.com>
2024-11-10 15:05:05 +0300

    last commit v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home"
-import Gallery from "./pages/galleryPage"
+import Gallery from "./pages/GalleryPage"
 import Service from "./pages/ServicePage"
 import AdminPage from "./pages/AdminPage";
 
